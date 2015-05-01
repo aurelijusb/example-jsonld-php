f981ba33c1ce78168c00a126c22959a318c86077
Aurelijus Banelis <aurelijus@banelis.lt>
2015-05-01 14:01:57 +0300

    Using cached context for known external hosts
    Without changing library itself.

--- index.php
+++ index.php
@@ -6,14 +6,19 @@ use \ML\JsonLD\JsonLD;
 use \ML\JsonLD\NQuads;
 
 $file = 'test.json';
+$data = file_get_contents($file);
+$context = trim(file_get_contents('context.jsonld'), '{}');
+$inlineContext = str_replace('"@context": "http://auginte.com/ns/s.jsonld"', $context, $data);
 
+echo '<pre>' . htmlspecialchars($data) . '</pre><hr/>';
+echo '<pre>' . htmlspecialchars($inlineContext) . '</pre><hr/>';
 
-$expanded = JsonLD::expand($file);
+$expanded = JsonLD::expand($inlineContext);
 $expandedJson = JsonLD::toString($expanded, true);
 echo '<pre>' . htmlspecialchars($expandedJson) . '</pre><hr/>';
 
 
-$quads = JsonLD::toRdf($file);
+$quads = JsonLD::toRdf($inlineContext);
 $nquads = new NQuads();
 $serialized = $nquads->serialize($quads);
 print '<pre>' . htmlspecialchars($serialized) . '</pre><hr/>';
